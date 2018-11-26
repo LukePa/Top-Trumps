a7805715b25711d35dc329403b3995382c1347c4
LukePa <45145830+LukePa@users.noreply.github.com>
2018-11-26 14:59:36 +0000

    These are the cards I made, you can add or remove them as you want.

---
+++ card_list.py
@@ -0,0 +1,5 @@
+import base
+#Name, toughness, shooting, melee, leadership
+cards = [base.Card("Rick",5,7,4,9), base.Card("Glenn",3,6,6,7),
+         base.Card("Daryl",6,10,5,2), base.Card("Neagan",6,2,8,10),
+         base.Card("Carol",4,8,5,5), base.Card("Maggie",3,5,5,8)]
